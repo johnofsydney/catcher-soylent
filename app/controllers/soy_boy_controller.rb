ffb9129765634fceeca59351ac47c17cdc79e9a4
John Coote <john.coote@sherpa.net.au>
2023-10-28 11:03:12 +1100

    make controller to ingest payload from pitcher

--- app/controllers/soy_boy_controller.rb
+++ app/controllers/soy_boy_controller.rb
@@ -1,2 +1,37 @@
 class SoyBoyController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def update
+    @document = Document.find_by(token: document_params[:token])
+
+    if @document.present?
+      respond_to do |format|
+        if @document.update(link: document_params[:link])
+          format.html { redirect_to document_url(@document), notice: "Document was successfully updated." }
+          format.json { render :show, status: :ok, location: @document }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @document.errors, status: :unprocessable_entity }
+        end
+      end
+    else
+      @document = Document.new(document_params)
+
+      respond_to do |format|
+        if @document.save
+          format.html { redirect_to document_url(@document), notice: "Document was successfully created ." }
+          format.json { render :show, status: :ok, location: @document }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @document.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+  end
+
+  private
+    # Only allow a list of trusted parameters through.
+    def document_params
+      params.except(:soy_boy).permit(:description, :link, :key, :token)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :documents
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+    post '/soy-boy' => 'soy_boy#update'
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
