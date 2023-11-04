34919db0208f3a59b2c391257bffcf58b20594d9
John Coote <john.coote@sherpa.net.au>
2023-11-04 13:46:06 +1100

    make documents the home page

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,2 +1,5 @@
 class Document < ApplicationRecord
+  def is_image?
+    self.link.ends_with?('.jpg', '.jpeg', '.png', '.gif', '.avif')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
     post '/soy-boy' => 'soy_boy#update'
   # Defines the root path route ("/")
-  root "home#index"
+  root "documents#index"
 end
